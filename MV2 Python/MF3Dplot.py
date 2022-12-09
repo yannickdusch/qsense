7945659551d534fc10fe78038d53c44291addc83
Noé Pigret <92740883+Gipeon12@users.noreply.github.com>
2022-12-09 12:52:50 +0100

    Add files via upload

--- MV2 Python/MF3Dplot.py
+++ MV2 Python/MF3Dplot.py
@@ -34,33 +34,84 @@ def ChangeBase(MV2field) :  # Changes base by using the projection of the sensor
 
 class MF3D :  # Real time 3D plot of the magnetic field vector
     
-    def __init__(self, port = 'COM3') :
+    def __init__(self, port = 'COM3', multi = False) :  # 'multi = True' activates the multiplotting (3D and 2D spherical angles)
+        self.multi = multi
         self.ser = serial.Serial()
         self.ser.port = 'COM3'
         self.ser.baudrate = 115200
         self.ser.timeout = 0
         self.ser.open()
-        self.fig = plt.figure(figsize = (10,10))
-        self.ax = plt.axes(projection = '3d')
-        self.ax.set_xlim([-50,50])
-        self.ax.set_ylim([-50,50])
-        self.ax.set_zlim([-50,50])
-        self.ax.quiver(start[0],start[1],start[2],10,0,0,color = 'b')  # X axis (colored version : R)
-        self.ax.quiver(start[0],start[1],start[2],0,10,0,color = 'k')  # Y axis (colored version : G)
-        self.ax.quiver(start[0],start[1],start[2],0,0,10,color = 'k')  # Z axis (colored version : B)
-        self.ax.view_init(15,-60)  # Args = angles (°)
+        if self.multi :
+            self.fig = plt.figure(figsize = (8,14))
+            self.ax1 = plt.subplot2grid((3, 2), (0, 0), colspan=2, rowspan=2, projection='3d')
+            self.ax2 = plt.subplot2grid((3, 2), (2, 0), polar = True)
+            self.ax3 = plt.subplot2grid((3, 2), (2, 1), polar = True)
+            self.ax1.set_xlim([-50,50])
+            self.ax1.set_ylim([-50,50])
+            self.ax1.set_zlim([-50,50])
+            self.ax2.set_rlim([0,50])
+            self.ax2.set_theta_zero_location("E")
+            self.ax3.set_rlim([0,50])
+            self.ax3.set_thetalim([0,m.pi])
+            self.ax3.set_theta_zero_location("N")  # Put 0° on the right
+            self.ax3.set_theta_direction(-1)  # Set clockwise direction
+            self.ax1.view_init(15,-60)  # Args = angles (°)
+            self.ax2.grid(True)
+            self.ax3.grid(True)
+        else :
+            self.fig = plt.figure(figsize = (10,10))
+            self.ax = plt.axes(projection = '3d')
+            self.ax.set_xlim([-50,50])
+            self.ax.set_ylim([-50,50])
+            self.ax.set_zlim([-50,50])
+            self.ax.quiver(start[0],start[1],start[2],10,0,0,color = 'b')  # X axis (colored version : R)
+            self.ax.quiver(start[0],start[1],start[2],0,10,0,color = 'k')  # Y axis (colored version : G)
+            self.ax.quiver(start[0],start[1],start[2],0,0,10,color = 'k')  # Z axis (colored version : B)
+            self.ax.view_init(15,-60)  # Args = angles (°)
         self.anim = FuncAnimation(self.fig, self.animate, frames=100, interval=50)
     
-    def MF3Dplot(self, Bmoy) :
-        plt.cla()  # Clear the content already plotted without closing the figure (NB : plt.clf() clears the figure without closing the window)
+    def MF3Dplot(self, Bmoy) :  # Plots only the 3D field (higher frame rate)
+        self.ax.cla()  # Clear the content already plotted without closing the figure (NB : plt.clf() clears the figure without closing the window)
         self.ax.set_xlim([-50,50])
         self.ax.set_ylim([-50,50])
         self.ax.set_zlim([-50,50])
+        self.ax.set_title("3D Magnetic Field")
         self.ax.quiver(start[0],start[1],start[2],10,0,0,color = 'b')
         self.ax.quiver(start[0],start[1],start[2],0,10,0,color = 'k')
         self.ax.quiver(start[0],start[1],start[2],0,0,10,color = 'k')
         self.ax.quiver(start[0],start[1],start[2],Bmoy[0],Bmoy[1],Bmoy[2],color = 'r')
         self.ax.quiver(start[0],start[1],start[2],Bmoy[0],Bmoy[1],0)  # Projection of B on xy-plane
+    
+    def MFmultiplot(self, Bmoy) :  # Plots the field in 3D and shows the spherical coordinates in 2D plots
+        self.Bxy = (Bmoy[0]**2 + Bmoy[1]**2)**0.5
+        self.ax1.cla()  # Clear axes for the 3D figure
+        self.ax2.cla()  # Clear axes for the first 2D figure
+        self.ax3.cla()  # Clear axes for the second 2D figure
+        self.ax1.set_xlim([-50,50])
+        self.ax1.set_ylim([-50,50])
+        self.ax1.set_zlim([-50,50])
+        self.ax2.set_rlim([0,50])
+        self.ax2.set_theta_zero_location("E")
+        self.ax3.set_rlim([0,50])
+        self.ax3.set_thetalim([0,m.pi])
+        self.ax3.set_theta_zero_location("N")  # Put 0° on the right
+        self.ax3.set_theta_direction(-1)  # Set clockwise direction
+        self.ax1.set_title("3D Magnetic Field")
+        self.ax2.set_title("Spherical φ")
+        self.ax3.set_title("Spherical θ")
+        self.ax1.quiver(start[0],start[1],start[2],10,0,0,color = 'b')  # X axis
+        self.ax1.quiver(start[0],start[1],start[2],0,10,0,color = 'k')  # Y axis
+        self.ax1.quiver(start[0],start[1],start[2],0,0,10,color = 'k')  # Z axis
+        self.ax1.quiver(start[0],start[1],start[2],Bmoy[0],Bmoy[1],Bmoy[2],color = 'r')
+        self.ax1.quiver(start[0],start[1],start[2],Bmoy[0],Bmoy[1],0)  # Projection of B on xy-plane
+        self.ax2.quiver(0,0,10,0,color = 'b',scale_units='y',scale=2)  # X axis
+        self.ax2.quiver(0,0,0,10,color = 'k',scale_units='y',scale=2)  # Y axis
+        self.ax2.quiver(start[0],start[1],Bmoy[0],Bmoy[1],color = 'c',scale_units='y',scale=2)    
+        self.ax3.quiver(0,0,self.Bxy,0,color = 'c',scale_units='y',scale=2)  # Bxy
+        self.ax3.quiver(0,0,0,10,color = 'k',scale_units='y',scale=2)  # Z axis
+        self.ax3.quiver(start[0],start[1],self.Bxy,Bmoy[2],color = 'r',scale_units='y',scale=2)
+        self.ax2.grid(True)
+        self.ax3.grid(True)
     
     def animate(self, i) :
         lines = self.ser.readlines()
@@ -71,7 +122,10 @@ class MF3D :  # Real time 3D plot of the magnetic field vector
                     MV2data = line[1:-1].split(',')
                     MV2field = RawToField(MV2data)
                     field = ChangeBase(MV2field)
-                    self.MF3Dplot(field)
+                    if self.multi :
+                        self.MFmultiplot(field)
+                    else :
+                        self.MF3Dplot(field)
     
     def close(self) :
         self.ser.close()
