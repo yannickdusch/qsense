f96abf69baa0acb3d162b54189dad70b9a1fdc69
Minebox260 <39033036+Minebox260@users.noreply.github.com>
2023-02-06 15:10:50 +0100

    Updated main.py (Adding ODMR page + fixed missing "exports" dir creation)

--- Web App/main.py
+++ Web App/main.py
@@ -7,18 +7,31 @@
 #
 
 from nicegui import ui, app
-from app.pages.home import create_homepage
+from app.pages.home import create_home_page
 from app.pages.pico_ui import create_pico_ui
+from app.pages.odmr import create_odmr_ui
 import os
+import glob
 
 abs_file_path = os.path.abspath(__file__) # On récupère le chemin absolu du fichier actuel
 main_dir = os.path.dirname(abs_file_path) # On récupère le dossier racine de la web app
 resources_dir = os.path.join(main_dir, 'app/resources') # On se déplace jusqu'à resources en s'affranchissant du système de fichiers
 app.add_static_files('/resources', resources_dir) # On rajoute le dossier resources à l'application, pour qu'il soit accessible depuis /resources et qu'on puisse charger les images (pour ./app/pages/home.py)
+exports_dir = os.path.join(main_dir, 'app/exports')
+app.add_static_files('/exports', exports_dir) # On rajoute également le dossier exports, pour qu'on puisse y exporter les graphes
 
-create_homepage() # On créé la page d'accueil définie dans /app/pages/home.py
+exported_files_to_remove = glob.glob(exports_dir + "/*")
+for file in exported_files_to_remove: # On supprime les fichiers exportés précédemment, s'il y en a
+    try:
+        os.remove(file)
+    except OSError as err:
+        print("[ERR] Impossible de supprimer le fichier : {}".format(str(file)))
+        print(str(err))
+
+create_home_page() # On créé la page d'accueil définie dans /app/pages/home.py
 
 create_pico_ui() # On créé les autres pages de l'UI (Les pages ne sont exécutés que lorsqu'un utilisateur est dessus, grâce aux @ui.page)
+create_odmr_ui()
 
 # Lancement de l'UI
 ui.run()
